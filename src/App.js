a9efab39b58e8f71d5844e0ff7411a5ec774e59f
George Puryear <gm.puryear@yahoo.com>
2022-09-14 13:13:06 -0500

    using UseState hook to show changes when buttons are pressed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react";
 
+// A component is a function that returns UI (JSX)
+// we want to create components that occupy all three places (Header, Main, Footer)
+// To add properties to "props" object, we need to add them to where the components are being
+// So in this case, its the App function
+// Keys can help you keep your data in sync as your application state changes over time
+
+
+// component
+// function Header(props) {
+//     return (
+//         <header>
+//             <h1>{props.name} Kitchen</h1>
+//         </header>
+//     );
+// }
+
+// component
+// function Main(props) {
+//     return (
+//         <section>
+//             <p>We Serve the most {props.adjective} food around</p>
+//             {/*You can also just used direct links to pictures from web*/}
+//             {/*<img src={"https://github.com/gmpuryear.png"} height = {200}/>*/}
+//             <img src = {restaurant} height={200} alt="napkin and silverware at restaurant  table"/>
+//             <ul style = {{textAlign: "left"}}>
+//                 {props.dishes.map((dish) => (
+//                     <li key={dish.id}>{dish.title}</li>
+//                 ))}
+//             </ul>
+//         </section>
+//     )
+// }
+
+// // component
+// function Footer(props) {
+//     return (
+//         <footer>
+//             <p>Copyright {props.year}</p>
+//         </footer>
+//     )
+// }
+
+// *Not using dishes array anymore because we want to attach a key to each dish and make each its own object
+//         with properties id and title.
+// const dishes = ["Macaroni and Cheese", "Salmon", "Steak", "grilled chicken"];
+
+// returning dish objects with id and title as properties
+// const dishObjects = dishes.map((dish, i) => ({id: i, title: dish}))
+// console.log(dishObjects);
+
+// secret component that where only people who are authorized can see
+// function SecretComponent() {
+//     return <h1>Secret information for authorized users only</h1>
+// }
+
+// Can see without being logged in
+// function RegularComponent() {
+//     return <h1>Everyone can see this component.</h1>
+// }
+// The ',,' is skipping over the first 2 array elements and sets "mostImportant" to headlamp
+//         this is called "destructuring" where we give elements variable names
+// const [,, mostImportantItem] = ["boots", "tent", "headlamp"];
+// console.log(mostImportantItem);
+
+// this composes these components together to make a larger application
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // "emotion" is the state variable, "setEmotion" is a function that updates the state and we pass in what we want the
+            // the first state to be.
+    const [emotion, setEmotion] = useState("happy");
+
+    return (
+        <>
+            <h1>Current emotion is {emotion}.</h1>
+            <button onClick={() => setEmotion("happy")}>
+                Happy
+            </button>
+            <button onClick={() => setEmotion("frustrated")}>
+                Frustrate
+            </button>
+            <button onClick={() => setEmotion("enthusiastic")}>
+                Enthuse
+            </button>
+        </>
+    )
+        ;
 }
+
+/* if this is true, render secretComponent*/
+
+/*{authorized ? <SecretComponent /> : <RegularComponent />}*/
+
+
+// Notice that this is "className" and not "class"
+// instead of using the <div> wrappers, you could also just use <> </>
+// <div className="App">
+//     <Header name = "Matt"/>
+//     {/*Because were passing in the variable "dishes", we use curly braces*/}
+//     <Main adjective = "amazing" dishes = {dishObjects}/>
+//     <Footer year = {new Date().getFullYear()}/>
+// </div>
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Second App I created for practice which was rendered below
+// function AppTwo() {
+//     return <h1>This is the Second App</h1>;
+// }
+// We can render two components like this here and wrapped with a div element
+// instead of having a bunch of
 root.render(
-  <React.StrictMode>
+            // This does not add any elements to the DOM.
+    // <div>
+    //     <App />
+    //     <AppTwo />
+    // </div>
+
+    // instead of having a bunch of divs on a page you can wrap different components with <react.fragment>
+    // This does not add any elements to the DOM.
+    // <React.Fragment>
+    //     <App />
+    //     <AppTwo />
+    // </React.Fragment>
+
+    // This is short hand version for rendering two components with no other additions to the DOM
+    // <>
+    //     <App />
+    //     <AppTwo />
+    // </>
+
     <App />
-  </React.StrictMode>
+
+    // <React.StrictMode>
+    //   <App />
+    // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
